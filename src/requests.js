383e048b047f7a63ac6db509abcd66678e814851
abdelwhab <abdeebrahim17@gmail.com>
2023-01-18 03:28:24 +0300

    my

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "d32ba7bedf85cde7a1ee722e5cf1273b";
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+	fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
